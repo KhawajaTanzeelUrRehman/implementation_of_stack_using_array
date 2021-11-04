82d84d4cdd554d047346f14ae41659dafbaad6bc
KhawajaTanzeelUrRehman <77752933+KhawajaTanzeelUrRehman@users.noreply.github.com>
2021-11-05 02:48:02 +0500

    Add files via upload

---
+++ stackwitharray.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+class stack {
+    //class for the stack
+public:
+    int size;
+    int top;
+    char* arr;
+
+    stack() {
+        //Defualt constructor for the stack.
+        size = 300;
+        top = -1;
+        arr = new char[size];
+    }
+    //args constructor for the stack.
+    stack(int sizeofstack) {
+        size = sizeofstack;
+        top = -1;
+        arr = new char[size];
+    }
+
+    bool isFull() {
+        //function which check the stack is full or not.
+        if (top == size - 1) {
+            cout << "Full";
+        }
+        return top == size - 1;
+    }
+
+    bool isEmpty() {
+        //funciton that check the function is empty.
+        return top == -1;
+    }
+
+    void push(char value) {
+        //funciton to push element in the stack
+        if (isFull()) {
+            cout << "This Stack is full";
+        }
+        else {
+            top++;
+            arr[top] = value;
+        }
+    }
+
+    char pop() {
+        //funciton to pop element from the stack.
+        if (!isEmpty()) {
+            top--;
+            return arr[top + 1];
+        }
+        else {
+            return '!';
+        }
+    }
+
+    void clear() {
+        //function to deleter all entries
+        delete[] arr;
+    }
+
+    void peak() {
+        //funciton that print the top element of the stack.
+        if (!isEmpty()) {
+            cout << arr[top] << endl;
+        }
+    }
+};
